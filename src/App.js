3bb6bef2a91a99716ab2f58dab7ffa3ca90ea8c8
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2024-05-25 20:12:43 +0530

    learned about components and added styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import React from 'react'
+
+
+// this is card component
+function Card() {
+  return (
+    <div style={{ border: "solid black 2px", padding: "10px", margin: "10px", borderRadius: "20px" }}>
+      <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile"
+
+        style={{ borderRadius: '50%' }}
+      />
+
+      {/* bharat-shinde : bharatShinde
+          background-image: backgroundImage
+          background-size: backgroundSize
+       */}
+      <h1 style={{ color: '#010101', fontSize: '20px', marginTop: 0, marginBottom: "0px" }}>@rohit</h1>
+
+      <p style={{ marginTop: "0px" }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio, quam!</p>
+
+      <div style={{ backgroundColor: 'blue', color: 'white', padding: "10px", borderRadius: '10px', width: '50px' }}>follow</div>
+
+    </div >
+  )
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Card />
+      <Card />
+      <Card />
+
+
     </div>
+
   );
 }
 

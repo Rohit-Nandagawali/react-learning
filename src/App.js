22d8b717f96a7e25ffee6d9645820bf45985baf0
Rohit Nandagawali <nandagawalirohit143@gmail.com>
2024-05-25 20:49:08 +0530

    message added

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ function Card() {
       <img src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=100&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile"
 
         style={{ borderRadius: '50%' }}
+
       />
 
       {/* bharat-shinde : bharatShinde
           background-image: backgroundImage
           background-size: backgroundSize
+          added new code
        */}
       <h1 style={{ color: '#010101', fontSize: '20px', marginTop: 0, marginBottom: "0px" }}>@rohit</h1>
 
